bf1e2dbb533e9b739604aadf3af7bd5a00627926
Nikita Sharma <nikitasharma9010@gmail.com>
2017-10-29 02:20:01 +0530

    Merge files into dummy_data.py

--- dummy_data.py
+++ dummy_data.py
@@ -9,7 +9,7 @@ from database_setup import Base, Category, Item, User
 
 # Connect to database,
 # Create session
-engine = create_engine("sqlite:///item_catelog.db")
+engine = create_engine('sqlite:///item_catelog.db')
 # Bind schema constructs(mapper code) to engine
 Base.metadata.bind = engine
 # Create a configured Session class
@@ -17,6 +17,45 @@ DBSession = sessionmaker(bind=engine)
 # Create a session
 session = DBSession()
 
+# Add some users
+user1 = User(name="User1",
+             email="example1@example1.com",
+             picture="http://via.placeholder.com/100x100")
+session.add(user1)
+session.commit()
+
+user2 = User(name="User2",
+             email="example2@example2.com",
+             picture="http://via.placeholder.com/100x100")
+session.add(user2)
+session.commit()
+
+print("Users added!")
+
+# Add some categories
+category1 = Category(name="category1",
+                     user_id=1)
+session.add(category1)
+session.commit()
+
+category2 = Category(name="category2",
+                     user_id=1)
+session.add(category2)
+session.commit()
+
+category3 = Category(name="category3",
+                     user_id=1)
+session.add(category3)
+session.commit()
+
+category4 = Category(name="category4",
+                     user_id=1)
+session.add(category4)
+session.commit()
+
+print("Categories added!")
+
+# Add some items
 item1 = Item(name="item1",
              description="description1",
              category_id=1,
